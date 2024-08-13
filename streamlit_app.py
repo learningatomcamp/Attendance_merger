89c3875ad83d16a11cb6daec0b060f2c37cf58a8
Azeem <chaudhry.azeem0308@gmail.com>
2024-08-14 03:42:42 +0500

    added main functionality for one file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import time
+import numpy as np
+from datetime import datetime
+import re
 
 st.set_page_config(layout="wide")
 with open('ui.html', 'r') as f:
@@ -9,26 +12,55 @@ with open('ui.html', 'r') as f:
 components.html(html_content, height=100) 
 
 
+col1,col2 = st.columns(2)
+
+def parse_duration(duration_str):
+    minutes = 0
+    hours_match = re.search(r'(\d+)\s*hr', duration_str)
+    minutes_match = re.search(r'(\d+)\s*min', duration_str)
+    if hours_match:
+        hours = int(hours_match.group(1))
+        minutes += hours * 60
+    if minutes_match:
+        minutes += int(minutes_match.group(1))
+    return minutes
 
 
 def main ():
+    # available files (can be changed later)
     menu = ["AI","DS","ML","DA Gray","DA Black","DA white","DS6","DS7 Blue","DS7 Green"]
 
-    st.subheader("Attendance")
+    col1.subheader("Attendance")
 
-    datafile = st.file_uploader("Upload CSV",type = ['csv'])
+    datafile = col1.file_uploader("Upload CSV",type = ['csv'])
+    # loading data if file is uploaded.
     if datafile is not None:
         df= pd.read_csv(datafile)
-        st.dataframe(df)
-
-        choice = st.selectbox("Course", menu)
+        col1.dataframe(df) # confirming that the right file is uploaded
+        # choosing from the daily file.
+        choice = col1.selectbox("Course", menu)
+        current_date = col1.date_input("Enter the date",format="DD-MM-YYYY")
         if (st.button("Update")) and choice is not None:
-            progress_bar = st.progress(0)
 
-            for percent_complete in range(100):
-                time.sleep(0.001)  # Delay for each step
-                progress_bar.progress(percent_complete + 1)
+            if choice == "DA Black" :
+                original_df = pd.read_csv(r"C:\Users\Azeem\OneDrive\Documents\Projects\attendance_merger\DA Cohort 01(Black) - Trackerrr.csv")
+                
+                #######################processing the data######################
+                df["Name"] = (df['First name'] + df['Last name']).str.replace(' ', '').str.lower() 
+                # making it lower case to avoid erros 
+                original_df['Name_lower'] = original_df['Name'].str.replace(' ', '').str.lower() # removing spaces
 
+                #updating the duration column for the time the student attended the class
+                df['Duration'] =df['Duration'].apply(parse_duration)
+
+                original_df['duration'] = df['Duration']
+                # updating the attendance with 1 for present and 0 for absent
+                original_df[current_date] = np.where(original_df['Name_lower'].isin(df[df['Duration'] >= 45]['Name']),1,0)
+
+                # dropping the extra name column and keeping the original 
+                original_df.drop(columns=['Name_lower'], inplace=True)
+                
+                col1.dataframe(original_df) 
             st.success("Progress complete!")
 
 
